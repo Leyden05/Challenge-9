da000a698be85391c23afe10b555811c16d1ffae
leyden <fusionsoccer05@gmail.com>
2023-01-31 22:34:34 -0600

    added some functionality -- post tutoring session here

---
+++ dist/README.md
@@ -0,0 +1,22 @@
+# Test Title YAY
+
+## Description
+this is a thing
+
+## Table of Contents
+thing 1 thing 2 
+
+## Installation
+do thing 1
+
+## Usage
+thing 2 does thing 1
+
+## Contributions
+ahh
+
+## Tests
+nope
+
+## Improvements
+nope
---
+++ index.js
@@ -0,0 +1,78 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Description of your project:',
+        name: 'description',
+    }, 
+    {
+        type: 'input',
+        message: 'Describe your table of contents:',
+        name: 'toc'
+    },
+    {
+        type: 'input',
+        message: 'Describe the installation process:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Describe the usage:',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'What license do this application have?',
+        name: 'license',
+        choices: [
+            'Apache 2.0',
+            'MIT',
+            'Other',
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Describe the contributions outside parties made:',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'Describe the tests performed:',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Describe any improvements for future development:',
+        name: 'improvements'
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        error ? console.log(error) : console.log('File has been created!');
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        let template = generateMarkdown(answers);
+        // console.log(template);
+        writeToFile('./dist/README.md', template);
+    })
+}
+
+// Function call to initialize app
+init();
